9d92a5a2f2602bd2f666089055ea5f4521f887b3
Matt Todd <matt.c.todd@gmail.com>
2014-08-16 16:42:53 -0500

    added error handling

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -3,6 +3,8 @@ var testControllers = angular.module('testControllers', []);
 testControllers.controller('HomeCtrl', ['$scope', 'MoviesService', function($scope, MoviesService) {
    MoviesService.get().success(function(data) {
       $scope.movies = data;
+   }).error(function() {
+      $scope.errorMessage = 'Movies failed to load: ' + arguments;
    });
    $scope.sortOrder = 'release';
    $scope.reverse = true;
